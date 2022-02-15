64ffe4967f1372c946211497353ca51c38d90be1
Silajit Saha <sahasilajit3gmail.com>
2022-02-15 14:36:54 +0530

    Revert "Initial commit"
    This reverts commit dfeb5df9

--- Filmatopedia/movie/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ExtendedUser,Suggestion,Group,GroupMember
-
-# Register your models here.
-admin.site.register(ExtendedUser)
-admin.site.register(Suggestion)
-admin.site.register(GroupMember)
-admin.site.register(Group)
